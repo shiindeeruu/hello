6853b9d982997a7c3456eaee7fdea834ebf6eb5c
Hannah Shin <hshin9@huskers.unl.edu>
2021-08-26 23:11:08 +0000

    added major and name code

--- hello.c
+++ hello.c
@@ -1,1 +1,19 @@
-// Hannah 
+/**
+ * Author: Hannah Shin
+ * Date: 2021/08/26
+ * Class: CSE155E Chris Bourke
+ *
+ * A simple program to say my major
+ *
+ */
+ #include <stdlib.h>
+ #include <stdio.h>
+
+ int main(int argc, char **argv){
+
+     printf("Computer Science\n");
+     printf("Hannah Shin!\n");
+
+     return 0;
+
+ }
